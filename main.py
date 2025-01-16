ab7547379d299909528e25280d0a4d6c6515ad1c
lmraine <lraine@fhu.edu>
2025-01-16 11:53:07 -0600

    updated tests

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #REMOVE PASS AND FIX THE FUNCTION
-#change to test push and forks
 def sum_of_products(list1, list2):
     pass
 
+
 if __name__ == '__main__':
-   #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+   #Remove pass and test function here
+   pass
