979dda6d464020bcc1d707325f98e02b099ddee8
lmraine <lraine@fhu.edu>
2025-01-16 12:07:54 -0600

    removed tests

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 #REMOVE PASS AND FIX THE FUNCTION
+#change to test push and forks
 def sum_of_products(list1, list2):
-    pass
+    result = 0
+    if len(list1) != len(list2):
+        return "Error"
+    else:
+        for i in range(len(list1)):
+            result += list1[i] * list2[i]
+        return result
 
 
 if __name__ == '__main__':
-   #Remove pass and test function here
-   pass
+    l1 = input().split()
+    l2 = input().split()
+    sum_of_products(l1,l2)
+    
